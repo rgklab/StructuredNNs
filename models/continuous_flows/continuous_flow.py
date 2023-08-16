243078c95aaba2682d6def302a3ff7e2ae4debe0
Ian <ian.shi@mail.utoronto.ca>
2023-08-16 16:29:58 -0400

    WIP: adding CNF code.

--- models/continuous_flows/continuous_flow.py
+++ models/continuous_flows/continuous_flow.py
@@ -1,6 +1,13 @@
 import torch
+import torch.nn as nn
 
+from ..strNN import StrNN
 from ..normalizing_flow import NormalizingFlow
+
+
+class StrODENet(StrNN):
+    def forward(self, t, x):
+        return super().forward(x)
 
 
 class ContinuousFlow(NormalizingFlow):
@@ -13,3 +20,45 @@ class ContinuousFlow(NormalizingFlow):
 
     def invert(self, z: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
         pass
+
+
+class ContinuousFlowFactory():
+
+    def build_odenet(self,
+                     input_dim: int,
+                     hidden_dim: dict,
+                     adj_mat: torch.Tensor,
+                     opt_type: str) -> nn.Module:
+        """Constructs neural network used to model ODE dynamics function.
+
+        TODO
+        Args:
+            args: Model arguments.
+            adj_mat: Binary adjacency matrix of shape D x D.
+
+        Returns:
+            nn.Module: NN represent dynamics function.
+        """
+        odenet = None
+        return odenet
+
+    def build_cnf(self, args: dict, adj_mat: torch.Tensor) -> ContinuousFlow:
+        odenet = self.build_odenet(args["input_dim"], args["hidden_dim"], adj_mat)
+           
+        odefunc = layers.ODEfunc(
+            diffeq=odenet,
+            divergence_fn=args.divergence_fn
+        )
+        cnf = layers.CNF(
+            odefunc=odefunc,
+            T=args.time_length,
+            train_T=args.train_T,
+            solver=args.solver,
+        )
+
+        chain = [build_cnf() for _ in range(args.num_blocks)]
+        model = layers.SequentialFlow(chain)
+
+        set_cnf_options(args, model)
+
+        return model
--- models/strNN.py
+++ models/strNN.py
@@ -11,4 +11,14 @@ class MaskedLinear(nn.Module):
 
 
 class StrNN(nn.Module):
-    pass
+    def __init__(self,
+                 input_dim: int,
+                 hidden_dim: list[int],
+                 out_dim: int,
+                 activation: str,
+                 opt_type: str,
+                 opt_args: dict):
+        pass
+
+    def forward():
+        pass
