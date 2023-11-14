664528261a05efb0b1be84db0522c67dbffa381a
IanShi1996 <ian.shi@mail.utoronto.ca>
2023-11-14 18:03:45 -0500

    Amended package version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="strnn",
-    version="0.2.1",
+    version="0.0.1",
     author="RGKLab",
     description="PyTorch package for Structured Neural Networks.",
     long_description_content_type="text/markdown",
