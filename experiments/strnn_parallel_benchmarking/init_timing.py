3022f05df5158933e45623dceb5abca5eff1f5df
IanShi1996 <ian.shi@mail.utoronto.ca>
2024-04-25 17:23:59 -0400

    Reduced parallelization overhead.

--- experiments/strnn_parallel_benchmarking/init_timing.py
+++ experiments/strnn_parallel_benchmarking/init_timing.py
@@ -5,19 +5,23 @@ import numpy as np
 from strnn import StrNN
 
 n_trials = 5
+expansion = 5
+n_layer = 3
 dimensions = [10, 50, 100, 250, 500, 750, 1000, 1500, 2000, 3000]
-times = {d: [] for d in dimensions}
+
+times: dict[int, list[float]] = {d: [] for d in dimensions}
 
 for d in dimensions:
     print(d)
     for _ in range(n_trials):
         s = time()
-        A = np.random.uniform(0, 1, size=(d, d))
-        A = A > 0.5
+        A_samp = np.random.uniform(0, 1, size=(d, d))
+        A = A_samp > 0.5
 
-        StrNN(d, [d, d, d], d, opt_type="greedy", adjacency=A)
+        n_hidden = tuple([d * expansion] * n_layer)
+        StrNN(d, n_hidden, d, opt_type="greedy_parallel", adjacency=A)
         e = time()
-        print(e - s)
+        print(str(e - s) + ",")
         times[d].append(e - s)
 
 for k, v in times.items():
--- strnn/factorizers/greedy_parallel.py
+++ strnn/factorizers/greedy_parallel.py
@@ -6,6 +6,8 @@ import numpy as np
 from .greedy import GreedyFactorizer
 
 M1_global: ctypes.c_long
+M2_global: ctypes.c_long
+A_global: ctypes.c_long
 
 
 class GreedyParallelFactorizer(GreedyFactorizer):
@@ -31,6 +33,10 @@ class GreedyParallelFactorizer(GreedyFactorizer):
     ) -> tuple[np.ndarray, np.ndarray]:
         """Factorize adj_mtx into M1 * M2 using parallelization.
 
+        Matrices M2 and A only require read operations in child processes.
+        M1 can be handled without synchronization as each matrix element is
+        only written to once.
+
         Args:
             adj_mtx: adjacency structure, n_outputs x n_inputs
             n_hidden: number of units in this hidden layer
@@ -47,55 +53,72 @@ class GreedyParallelFactorizer(GreedyFactorizer):
         for i in range(n_hidden):
             M2[i, :] = A_nonzero[i % n_nonzero_rows]
 
-        # Construct shared memory block representing M1
-        M1_shared = mp.Array("d", adj_mtx.shape[0] * n_hidden)
+        n_out = adj_mtx.shape[0]
+        n_in = adj_mtx.shape[1]
+
+        # Construct shared memory blocks
+        M1_shared = mp.RawArray("d", n_out * n_hidden)
+        M2_shared = mp.RawArray("d", n_hidden * n_in)
+        A_shared = mp.RawArray("d", n_out * n_in)
 
         # Set to all ones
-        M1_np = np.frombuffer(memoryview(M1_shared.get_obj()))
-        M1_np = M1_np.reshape((adj_mtx.shape[0], n_hidden))
+        M1_np = np.frombuffer(memoryview(M1_shared))
+        M1_np = M1_np.reshape((n_out, n_hidden))
         M1_np[:] = 1
 
-        def init(M1_mem_block):
+        M2_np = np.frombuffer(memoryview(M2_shared))
+        M2_np = M2_np.reshape((n_hidden, n_in))
+        M2_np[:] = M2
+
+        A_np = np.frombuffer(memoryview(A_shared))
+        A_np = A_np.reshape((n_out, n_in))
+        A_np[:] = adj_mtx
+
+        def init(M1_mem_block, M2_mem_block, A_mem_block):
             global M1_global
+            global M2_global
+            global A_global
+
             M1_global = M1_mem_block
+            M2_global = M2_mem_block
+            A_global = A_mem_block
 
         with mp.Pool(
             initializer=init,
-            initargs=(M1_shared,),
+            initargs=(M1_shared, M2_shared, A_shared),
             processes=mp.cpu_count()
         ) as pool:
             pool.starmap(
                 self._factorize_row,
-                [(i, M2, adj_mtx) for i in range(adj_mtx.shape[0])]
+                [(i, (n_in, n_hidden, n_out)) for i in range(n_out)]
             )
 
-        M1_np = np.frombuffer(memoryview(M1_shared.get_obj()))
+        M1_np = np.frombuffer(memoryview(M1_shared))
         M1_np = M1_np.reshape((adj_mtx.shape[0], n_hidden))
 
         return M1_np, M2
 
-    def _factorize_row(
-        self,
-        idx: int,
-        M2: np.ndarray,
-        adj_mtx: np.ndarray
-    ):
+    def _factorize_row(self, idx: int, shapes: tuple[int, int, int]):
         """Process factorization associated with single row.
 
         Args:
             idx: Row index to factorize.
-            M1: Mask of shape (n_outputs x n_hidden) to factorize.
-            M2: Mask of shape (n_hidden x n_inputs) to factorize.
-            adj_mtx: Adjacency matrix to respect.
+            shapes: Tuple of (n_in, n_hidden, n_out).
         """
-        M1_local = np.frombuffer(memoryview(M1_global.get_obj()))
-        M1_local = M1_local.reshape((adj_mtx.shape[0], M2.shape[0]))
+        M1_local = np.frombuffer(memoryview(M1_global))
+        M1_local = M1_local.reshape((shapes[2], shapes[1]))
+
+        M2_local = np.frombuffer(memoryview(M2_global))
+        M2_local = M2_local.reshape((shapes[1], shapes[0]))
+
+        A_local = np.frombuffer(memoryview(A_global))
+        A_local = A_local.reshape((shapes[2], shapes[0]))
 
         # Find indices where A is zero on the ith row
-        Ai_zero = np.where(adj_mtx[idx, :] == 0)[0]
+        Ai_zero = np.where(A_local[idx, :] == 0)[0]
 
         # find row using closed-form solution
         # find unique entries (rows) in j-th columns of M2 where Aij = 0
-        row_idx = np.unique(np.where(M2[:, Ai_zero] == 1)[0])
+        row_idx = np.unique(np.where(M2_local[:, Ai_zero] == 1)[0])
 
         M1_local[idx, row_idx] = 0.0
